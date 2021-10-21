2feafb8197594578750422990eb834d8d89c6ae1
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2021-10-21 19:35:55 +0200

    ProjectSummary component prototype

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+  return (    
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the posters</p>
+        <p className="grey-text">2021-10-21, 19:24</p>
+      </div>
+    </div>
+  );
+}
+ 
+export default ProjectSummary;
