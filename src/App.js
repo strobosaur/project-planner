212ad3926cf702211145658246286a0c68b1dbba
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2021-10-21 20:22:21 +0200

    SignUp component prototype

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
           <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
