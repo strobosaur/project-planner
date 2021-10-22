1ad2115b471f763f8e2de97dbb969ec855ac89f0
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2021-10-22 11:08:55 +0200

    ProjectList dynamification through props & reduction

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
   return (    
     <div className="card z-depth-0 project-summary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{project.title}</span>
         <p>Posted by the posters</p>
         <p className="grey-text">2021-10-21, 19:24</p>
       </div>
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  projects: [
+    {id: '1', title: 'project 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {id: '2', title: 'project 2', content: 'Praesent vitae volutpat dui, ac accumsan dolor.'},
+    {id: '3', title: 'project 3', content: 'Maecenas bibendum lobortis semper. Maecenas dictum a ipsum in tempor.'}
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
   return state;
