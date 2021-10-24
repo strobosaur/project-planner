67246f4da673134a62e3f7e45e746dd1d8b126ce
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2021-10-24 16:40:05 +0200

    SignIn authentication (sort of) works

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
 const Navbar = () => {
   return ( 
@@ -12,7 +13,13 @@ const Navbar = () => {
         <SignedOutLinks />
       </div>
     </nav>
-   );
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
 }
  
-export default Navbar;
+export default connect(mapStateToProps)(Navbar);
